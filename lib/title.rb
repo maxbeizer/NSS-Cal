b3ead8b18667b9439751cb3344e60c403ea7eda9
maxbeizer <max.beizer@gmail.com>
2012-09-24 15:46:54 -0500

    added month names and month DNE

--- lib/title.rb
+++ lib/title.rb
@@ -1,9 +1,17 @@
 class Title
-  attr_reader :month
-  attr_reader :year
+  attr_reader :month_num, :year
+
+  MONTH_NAME_ARRAY = [nil, "January", "February", 
+                "March", "April", "May", "June", "July", "August", 
+                "September", "October", "November", "December"]
+
+  def month_name
+    raise ArgumentError, "Lousy Smarch Weather! That month DNE" if MONTH_NAME_ARRAY[@month_num].nil?
+    MONTH_NAME_ARRAY[@month_num]
+  end
   
-  def initialize(month, year)
-    @month = month
+  def initialize(month_num, year)
+    @month_num = month_num
     @year = year
   end
 end
--- test/test_title.rb
+++ test/test_title.rb
@@ -5,7 +5,24 @@ class TitleTest < Test::Unit::TestCase
 
   def test_01_title_takes_params
     title = Title.new(12, 2012)
-    assert_equal(12, title.month)
+    assert_equal(12, title.month_num)
+    assert_equal(2012, title.year)
   end
 
+  def test_02_title_params_link_month_name
+    title = Title.new(12, 2012)
+    assert_equal("December", title.month_name)
+  end
+
+  def test_02a_title_params_link_month_name_alt
+    title = Title.new(4, 2012)
+    assert_equal("April", title.month_name)
+  end
+
+  def test_03_title_month_DNE
+    title = Title.new(13, 2012)
+    assert_raise ArgumentError do
+      title.month_name
+    end
+  end
 end
