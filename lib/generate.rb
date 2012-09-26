1586a7ca1832b081ab84a577ac4375f44ca47330
maxbeizer <max.beizer@gmail.com>
2012-09-26 10:40:33 -0500

    month class filled out with tests passing

--- lib/generate.rb
+++ lib/generate.rb
@@ -2,6 +2,8 @@ require "zeller_alg"
 require "day_names"
 require "title"
 
+DAY_OF_MONTH = 1
+
 class Month
 attr_reader :month, :year
 
@@ -10,15 +12,21 @@ attr_reader :month, :year
     @year = year
   end
 
-  def month_name month
-    Title.new.month_name(month)
+  def month_name 
+    Title.new(@month, @year).month_name
   end
   
   def days_in_month
-
+    if !(@year % 100 == 0) && @year % 4 == 0 || @year % 400 == 0
+      leap = 29
+    else
+      leap = 28
+    end
+    days_in_month = [nil, 31, leap, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_in_month[@month]
   end
 
   def day_of_week
-    ZellerAlg.day_of_week
+    ZellerAlg.day_of_week(@month, DAY_OF_MONTH, @year)
   end
 end
--- test/test_generate.rb
+++ test/test_generate.rb
@@ -3,8 +3,47 @@ require 'generate'
 
 class GenerateTest < Test::Unit::TestCase
 
-  def test_01_output
+  def test_01_month_name
     m = Month.new(9, 2012)
-    assert_equal "September" , m.month.to_s
+    n = Month.new(10, 2012)
+    o = Month.new(11, 2012)
+    p = Month.new(12, 2012)
+    assert_equal "September" , m.month_name
+    assert_equal "October" , n.month_name
+    assert_equal "November" , o.month_name
+    assert_equal "December" , p.month_name
+  end
+
+  def test_02_day_of_week
+    m = Month.new(9, 2012)
+    n = Month.new(10, 2012)
+    o = Month.new(1, 1812)
+    p = Month.new(12, 2912)
+    assert_equal 0 , m.day_of_week
+    assert_equal 2 , n.day_of_week
+    assert_equal 4 , o.day_of_week
+    assert_equal 5 , p.day_of_week
+  end
+
+  def test_03_days_in_month
+    m = Month.new(9, 2012)
+    n = Month.new(10, 2012)
+    o = Month.new(1, 1812)
+    p = Month.new(12, 2912)
+    assert_equal 30 , m.days_in_month
+    assert_equal 31 , n.days_in_month
+    assert_equal 31 , o.days_in_month
+    assert_equal 31 , p.days_in_month  
+  end
+
+  def test_03a_days_in_month_leap_months
+    m = Month.new(2, 2012)
+    n = Month.new(2, 2011)
+    o = Month.new(2, 1800)
+    p = Month.new(2, 2000)
+    assert_equal 29 , m.days_in_month
+    assert_equal 28 , n.days_in_month
+    assert_equal 28 , o.days_in_month
+    assert_equal 29 , p.days_in_month  
   end
 end
