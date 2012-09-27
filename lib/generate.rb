4cf56337c271fb93d645d2e3b5507ab12169ab49
maxbeizer <max.beizer@gmail.com>
2012-09-27 11:30:08 -0500

    refactored title spacing, weeks array broken

--- lib/generate.rb
+++ lib/generate.rb
@@ -16,16 +16,8 @@ attr_reader :month, :year
   end
 
   def title_spacing
-    case
-    when month_name.length >= 8
-      print "   "
-    when month_name.length > 5
-      print "    "
-    when month_name.length > 3
-      print "     "
-    else
-      print "      "   
-    end
+    title = month_name + " " + @year.to_s
+    title.center(20)
   end
 
   def day_names
@@ -44,25 +36,31 @@ attr_reader :month, :year
 
   def days_array
     days_array = []
-    1.upto(days_in_month) {|i| days_array.push(i.inspect)}
+    1.upto(days_in_month) {|i| days_array.push(i.inspect + " ")}
     days_array[0..8].each {|j| j.prepend(" ")}
     days_array.unshift("               ") if day_of_week == 0
     (day_of_week-1).times {|i| days_array.unshift("   ")}    
     days_array
-
-    #IDEA: create empty array. use upto and map spaces onto 
-    #      single digits and spaces (using select! ?). 
-    #      Insert into a long array. take method.
-    #      make new array for zeller days and flatten.
-    #      join will return the string for each array .join(" ")
-
   end
-  
+
+  def weeks_array
+    weeks_array = []
+    weeks_array.push(days_array[0..6])
+    weeks_array.push(days_array[7..13])
+    weeks_array.push(days_array[14..20])
+    weeks_array.push(days_array[21..27])
+    weeks_array.push(days_array[28..30]) unless days_array[28].nil?
+    weeks_array[0].each {|i| print i}
+    weeks_array[1].each {|i| print i}
+    weeks_array[2].each {|i| print i}
+    weeks_array[3].each {|i| print i}
+    weeks_array[4].each {|i| print i}
+  end
 
   def to_s
-    print title_spacing
-    puts month_name + " " + @year.to_s
+    puts title_spacing
+    # puts month_name + " " + @year.to_s
     puts day_names
-    puts days_array
+    puts weeks_array
   end
 end
