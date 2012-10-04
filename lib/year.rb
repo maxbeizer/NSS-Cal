b4eba472e924ae4c38056cb555f5db67da96211c
maxbeizer <max.beizer@gmail.com>
2012-10-03 22:23:28 -0500

    added year and month names with spacing

--- lib/year.rb
+++ lib/year.rb
@@ -5,12 +5,26 @@ require_relative "generate"
 class Year
 attr_reader :year
 
-  def initialize year
+  def initialize (year)
     @year = year
   end
 
   def year_spacing
     title = @year.to_s
     title.center(64)
+  end
+
+  def month_spacing
+    month_groups
+  end  
+
+  def month_names (start, finish)
+    i = start
+    month_groups = []
+    until i > finish
+      month_groups << Title.new(i, @year).month_name.center(20) + "  "
+      i += 1
+    end
+    month_groups.join
   end
 end
--- test/test_year.rb
+++ test/test_year.rb
@@ -11,6 +11,25 @@ class YearTest < Test::Unit::TestCase
   def test_02_year_title_centered_spacing
     y = Year.new(2012)
     assert_equal "                              2012                              ", y.year_spacing    
+  end
 
-   end
+  def test_03_print_first_3_month_names
+    y = Year.new(2012)
+    assert_equal "      January               February               March          ", y.month_names(1, 3) 
+  end
+
+  def test_03a_print_names_months_4_thru_6
+    y = Year.new(2012)
+    assert_equal "       April                  May                   June          ", y.month_names(4, 6) 
+  end
+
+  def test_03b_print_names_months_7_thru_9
+    y = Year.new(2012)
+    assert_equal "        July                 August              September        ", y.month_names(7, 9) 
+  end
+
+  def test_03c_print_names_months_10_thru_12
+    y = Year.new(2012)
+    assert_equal "      October               November              December        ", y.month_names(10, 12) 
+  end
 end
