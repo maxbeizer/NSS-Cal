af6663ccc66f1ee0a08c05d758320ac8530dc9c3
maxbeizer <max.beizer@gmail.com>
2012-10-04 18:39:51 -0500

    added ugly long implmentation and changed cal.rb to reflect one ARGV

--- lib/cal.rb
+++ lib/cal.rb
@@ -1,5 +1,12 @@
 require_relative "generate"
+require_relative "year"
 
-month = ARGV[0].to_i
-year = ARGV[1].to_i
-puts Month.new(month, year).print_month
+
+if  ARGV.length == 1
+  year = ARGV[0].to_i
+  puts Year.new(year).print_year	
+else
+  month = ARGV[0].to_i
+  year = ARGV[1].to_i
+  puts Month.new(month, year).print_month
+end
--- lib/year.rb
+++ lib/year.rb
@@ -50,7 +50,7 @@ attr_reader :year
     first_block
   end
 
-  def flatten_block
+  def flatten_first_block
     i = 0
     lines = []
     while i*3 < first_block.length
@@ -59,5 +59,90 @@ attr_reader :year
       i += 1
     end
     lines
+  end
+
+  def second_block 
+#this is ridiculously ugly, but could be translated into something that works
+    second_block = []
+    w = 0
+    until w > 4
+      m = 4
+      until m > 6
+        second_block << Month.new(m, @year).weeks_array[w].first(7).push(" ")
+        m+= 1
+      end
+      w += 1
+    end
+    second_block
+  end
+
+  def flatten_second_block
+    i = 0
+    lines = []
+    while i*3 < second_block.length
+      line = second_block[i*3..i*3+2].flatten.join
+      lines << line
+      i += 1
+    end
+    lines
+  end
+
+  def third_block 
+#this is ridiculously ugly, but could be translated into something that works
+    third_block = []
+    w = 0
+    until w > 4
+      m = 7
+      until m > 9
+        third_block << Month.new(m, @year).weeks_array[w].first(7).push(" ")
+        m+= 1
+      end
+      w += 1
+    end
+    third_block
+  end
+
+  def flatten_third_block
+    i = 0
+    lines = []
+    while i*3 < third_block.length
+      line = third_block[i*3..i*3+2].flatten.join
+      lines << line
+      i += 1
+    end
+    lines
+  end
+
+  def fourth_block 
+#this is ridiculously ugly, but could be translated into something that works
+    fourth_block = []
+    w = 0
+    until w > 4
+      m = 10
+      until m > 12
+        fourth_block << Month.new(m, @year).weeks_array[w].first(7).push(" ")
+        m+= 1
+      end
+      w += 1
+    end
+    fourth_block
+  end
+
+  def flatten_fourth_block
+    i = 0
+    lines = []
+    while i*3 < fourth_block.length
+      line = fourth_block[i*3..i*3+2].flatten.join
+      lines << line
+      i += 1
+    end
+    lines
+  end
+
+  def print_year
+    puts year_spacing
+    puts month_names(1, 3)
+    puts day_names
+    puts flatten_first_block
   end
 end
